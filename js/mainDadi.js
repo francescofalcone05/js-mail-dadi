01d82fd4bdbdca28ecc4640a307c3a860b0b2d94
francescofalcone05 <ciccio_97@live.it>
2024-04-22 18:30:48 +0300

    mail-compleated

--- js/mainDadi.js
+++ js/mainDadi.js
@@ -4,27 +4,30 @@ Genera due numeri random tra 1 e 6 (inclusi), uno per il giocatore e uno per il 
 Stabilisci il vincitore, in base a chi ha ottenuto il punteggio più alto.
 */
 
+
+//inizializzo i vari valori che mi serviranno
 const sendButton = document.getElementById('start')
 const plNr = document.getElementById('pl')
 const cpNr = document.getElementById('cp')
 const wiNa = document.getElementById('wn')
 
 
-
+//al click del bottone
 sendButton.addEventListener('click', function() {
 
 
-
+//genera un numero random da 1 a 6 per il player
 let player = (Math.floor(Math.random ()*6) +1)
 console.log(player)
 plNr.append(player)
 
+//genera un numero random da 1 a 6 per il Computer
 let computer = (Math.floor(Math.random ()*6) +1)
 console.log(computer)
 cpNr.append(computer)
 
 
-
+//uso if, per stabilire il vincitore e stampare in pagina i risultati
 if (player < computer) {
 
     console.log(`vince computer`)
---
+++ js/mainMail.js
@@ -0,0 +1,46 @@
+/*
+Chiedi all’utente la sua email 
+per poi controllare che sia nella lista di chi può accedere.
+Stampa un messaggio SOLO SE l'utente è in lista.
+*/
+
+//creo una lista
+let contenitore = [
+    'Francesco',
+    'Giovanni',
+    'Carmelo',
+    'Fabio'
+]
+console.log(contenitore)
+
+//chiedo il nome al cliente 
+let userName = prompt(`inserisci il tuo nome per verificare l'accesso`)
+
+//verifico se il userName=qualcosa in contenitore
+if (userName == contenitore[0]){
+
+    alert(`puoi accedere`)
+
+} else if (userName == contenitore[1]){
+
+    alert(`puoi accedere`)
+
+} else if (userName == contenitore[2]){
+
+    alert(`puoi accedere`)
+
+
+} else if (userName == contenitore[3]){
+
+    alert(`puoi accedere`)
+
+
+} else {
+
+    alert(`non puoi accedere`)
+}
+
+/* nel caso in cui il nome utente corrispondera' ad uno dei nomi dell'array, 
+ricevera' un alert con scritto che puo' accedere
+mentre se non dovesse corrispondere a nessuno dei nomi in array,
+ricevera' un alert con scritto che non puo' accedere */
